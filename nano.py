6006bc53a0f32fec712916ed86ebb1acabec203c
Kiril Misnikov <156070238+lime360@users.noreply.github.com>
2024-01-31 13:52:48 +0200

    new routing feature

--- nano.py
+++ nano.py
@@ -9,7 +9,7 @@ def group():
 @group.command(help="Reads a feed.")
 @click.argument("url")
 def read(url):
-    feed = json.loads(requests.get(url + "/nanoblog.php").text)
+    feed = json.loads(requests.get(url + "/nanoblog").text)
     click.echo(str(len(feed)) + " posts on " + url)
     for item in feed:
         click.echo(item["date"] + " - " + item["content"])
@@ -18,7 +18,7 @@ def read(url):
 @click.argument("url")
 @click.argument("content")
 def post(url, content):
-    if requests.post(url + "/post.php", data={'content': content}).ok:
+    if requests.post(url + "/post", data={'content': content}).ok:
        click.echo("Successfully made a post!")
     else:
        click.echo("Couldn't make a post.")
@@ -28,7 +28,7 @@ def post(url, content):
 @click.argument("content")
 @click.argument("postid")
 def edit(url, content, postid):
-    if requests.post(url + "/edit.php?id=" + postid, data={'content': content}).ok:
+    if requests.post(url + "/edit?id=" + postid, data={'content': content}).ok:
        click.echo("Post #" + postid + " has been edited.")
     else:
        click.echo("Couldn't edit a post.")
@@ -39,7 +39,7 @@ def edit(url, content, postid):
 def delete(url, postid):
     yorn = input("Are you sure? (Y/N)")
     if yorn == "y":
-        if requests.get(url + "/delete_post.php?id=" + postid).ok:
+        if requests.get(url + "/delete?id=" + postid).ok:
             click.echo("Post #" + postid + " has been deleted.")
         else:
             click.echo("Couldn't delete a post.")
